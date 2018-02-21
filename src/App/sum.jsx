9ea050637ca7b1e60ad75a2d463eb944cf674c37
Edmundo Rubio <zerphyr.r@gmail.com>
2018-02-20 19:35:18 -0700

    update onChange

--- src/App/sum.jsx
+++ src/App/sum.jsx
@@ -2,19 +2,28 @@ import React, { Component } from 'react'
 import './style.css'
 
 export default class extends Component{
+    constructor(props){
+        super(props)
+        this.state = { num1: '', num2: '', total: '' }
+    }
+
+    exe1(){
+        console.log("Hello from Exe1")
+    }
+
     render(){
         return(
             <div className="main-cont">
 
                 <h1>Sum Numbers</h1>
 
-                <input type="text"/>
+                <input type="text" value={this.state.num1} onChange={ this.exe1.bind(this) } />
                 <br/><br/>
 
-                <input type="text"/>
+                <input type="text" />
                 <br/><br/>
 
-                <input type="submit"/>
+                <input type="submit" onClick={this.exe1.bind(this)}/>
                 <br/><br/>
 
                 <input type="text"/>
